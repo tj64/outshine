4efc352bb84be0f59328b1f2c64703e9bea0bf21
Jonas Bernoulli <jonas@bernoul.li>
2013-03-07 15:01:57 +0100

    follow header conventions,
    where it doesn't conflict with outshine improvements.

--- outshine.el
+++ outshine.el
@@ -2,10 +2,27 @@
 
 ;; ** Copyright
 
-;; Copyright (C) 2013 Thorsten Jolitz
+;; Copyright (C) 2013  Thorsten Jolitz
+
+;; Author: Thorsten Jolitz <tjolitz AT gmail DOT com>
+;; Version:
+;; Homepage: https://github.com/tj64/outshine
+;; Keywords: outlines
+
 ;; This file is not (yet) part of GNU Emacs
 
-;; Contact: Thorsten Jolitz  <tjolitz AT gmail DOT com>
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; For a full copy of the GNU General Public License
+;; see <http://www.gnu.org/licenses/>.
 
 ;; ** Credits
 
