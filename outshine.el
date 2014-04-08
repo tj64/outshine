0b53523277581a17d3c2ed332271f155442aa622
tj <tj@data-driven.de>
2014-04-08 12:23:30 +0200

    Source blocks in comment-section converted into example blocks.

--- outshine.el
+++ outshine.el
@@ -25,97 +25,104 @@
 
 ;;;;; About outshine
 
-;; [NOTE: For the sake of adding this library to MELPA, headlines had to be
-;; converted back from 'Org-mode style' to 'oldschool', and a few extra lines
-;; of required information had to be added on top of the MetaData section -
-;; just to comply with the required file formatting. All outshine, outorg and
-;; navi-mode functionality still works with this file. See my
+;; [NOTE: For the sake of adding this library to MELPA, headlines
+;; had to be converted back from 'Org-mode style' to 'oldschool',
+;; and a few extra lines of required information had to be added on
+;; top of the MetaData section - just to comply with the required
+;; file formatting. All outshine, outorg and navi-mode functionality
+;; still works with this file. See my
 ;; [[https://github.com/tj64/iorg][iOrg]] repository for examples of
 ;; Emacs-Lisp and PicoLisp files structured 'the outshine way'.]
  
-;; This library merges, modifies and extends two existing extension-libraries
-;; for `outline' (minor) mode: `outline-magic' (by Carsten Dominik) and
-;; `out-xtra' (by Per Abrahamsen). It offers all the functionality of
-;; `outline-magic' (with some tiny changes) and parts of the functionality of
-;; `out-xtra', together with some new features and ideas.
+;; This library merges, modifies and extends two existing
+;; extension-libraries for `outline' (minor) mode: `outline-magic'
+;; (by Carsten Dominik) and `out-xtra' (by Per Abrahamsen). It
+;; offers all the functionality of `outline-magic' (with some tiny
+;; changes) and parts of the functionality of `out-xtra', together
+;; with some new features and ideas.
 
-;; See `outline-magic.el' (https://github.com/tj64/outline-magic) for
-;; detailled instructions on usage of the additional outline functions
-;; introduced by `outline-magic'.
+;; See `outline-magic.el' (https://github.com/tj64/outline-magic)
+;; for detailled instructions on usage of the additional outline
+;; functions introduced by `outline-magic'.
 
-;; Furthermore, `outshine.el' includes functions and keybindings from
-;; `outline-mode-easy-bindings' (http://emacswiki.org/emacs/OutlineMinorMode).
-;; Unfortunately, no author is given for that library, so I cannot credit the
-;; person who wrote it.
+;; Furthermore, `outshine.el' includes functions and keybindings
+;; from `outline-mode-easy-bindings'
+;; (http://emacswiki.org/emacs/OutlineMinorMode).  Unfortunately, no
+;; author is given for that library, so I cannot credit the person
+;; who wrote it.
 
-;; Outshine's main purpose is to make `outline-minor-mode' more similar to
-;; outline-navigation and structure-editing with (the one-and-only)
-;; `Org-mode'. Furthermore, as additional but quite useful features, correctly
-;; structured outshine-buffers enable the use of `outorg.el' (subtree editing
-;; in temporary Org-mode buffers) and `navi-mode.el' (fast navigation and
-;; remote-control via modified occur-buffers).
+;; Outshine's main purpose is to make `outline-minor-mode' more
+;; similar to outline-navigation and structure-editing with (the
+;; one-and-only) `Org-mode'. Furthermore, as additional but quite
+;; useful features, correctly structured outshine-buffers enable the
+;; use of `outorg.el' (subtree editing in temporary Org-mode
+;; buffers) and `navi-mode.el' (fast navigation and remote-control
+;; via modified occur-buffers).
 
 ;;;;; Installation
 
-;; Download `outshine.el' and copy it to a location where Emacs can find it,
-;; and use this in your '.emacs' to get started:
+;; Download `outshine.el' and copy it to a location where Emacs can
+;; find it, and use this in your '.emacs' to get started:
 
-;; # #+begin_src emacs-lisp
-;; #   (require 'outshine)
-;; #   (add-hook 'outline-minor-mode-hook 'outshine-hook-function)
-;; # #+end_src
+;; #+begin_example
+;;   (require 'outshine)
+;;   (add-hook 'outline-minor-mode-hook 'outshine-hook-function)
+;; #+end_example
 
-;; If you like the functions and keybindings for 'M -<<arrow-key>>' navigation
-;; and visibility cycling copied from `outline-mode-easy-bindings', you might
-;; want to put the following code into your Emacs init file to have the same
-;; functionality/keybindings available in Org-mode too, overriding the less
-;; frequently used commands for moving and promoting/demoting subtrees:
+;; If you like the functions and keybindings for 'M -<<arrow-key>>'
+;; navigation and visibility cycling copied from
+;; `outline-mode-easy-bindings', you might want to put the following
+;; code into your Emacs init file to have the same
+;; functionality/keybindings available in Org-mode too, overriding
+;; the less frequently used commands for moving and
+;; promoting/demoting subtrees:
 
-;; # #+begin_src emacs-lisp
-;; #   (add-hook 'org-mode-hook
-;; #             (lambda ()
-;; #               ;; Redefine arrow keys, since promoting/demoting and moving
-;; #               ;; subtrees up and down are less frequent tasks then
-;; #               ;; navigation and visibility cycling
-;; #               (when (require 'outshine nil 'NOERROR)
-;; #                 (org-defkey org-mode-map
-;; #                             (kbd "M-<left>") 'outline-hide-more)
-;; #                 (org-defkey org-mode-map
-;; #                             (kbd "M-<right>") 'outline-show-more)
-;; #                 (org-defkey org-mode-map
-;; #                             (kbd "M-<up>") 'outline-previous-visible-heading)
-;; #                 (org-defkey org-mode-map
-;; #                             (kbd "M-<down>") 'outline-next-visible-heading)))
-;; #             'append)
-;; # #+end_src
+;; #+begin_example
+;;   (add-hook 'org-mode-hook
+;;             (lambda ()
+;;               ;; Redefine arrow keys, since promoting/demoting and moving
+;;               ;; subtrees up and down are less frequent tasks then
+;;               ;; navigation and visibility cycling
+;;               (when (require 'outshine nil 'NOERROR)
+;;                 (org-defkey org-mode-map
+;;                             (kbd "M-<left>") 'outline-hide-more)
+;;                 (org-defkey org-mode-map
+;;                             (kbd "M-<right>") 'outline-show-more)
+;;                 (org-defkey org-mode-map
+;;                             (kbd "M-<up>") 'outline-previous-visible-heading)
+;;                 (org-defkey org-mode-map
+;;                             (kbd "M-<down>") 'outline-next-visible-heading)))
+;;             'append)
+;; #+end_example
 
-;; Add this to your .emacs if, e.g., you always want outshine for emacs-lisp
-;; buffers (recommended):
+;; Add this to your .emacs if, e.g., you always want outshine for
+;; emacs-lisp buffers (recommended):
 
-;; # #+begin_src emacs-lisp
-;; #   (add-hook 'emacs-lisp-mode-hook 'outline-minor-mode)
-;; # #+end_src
+;; #+begin_example
+;;   (add-hook 'emacs-lisp-mode-hook 'outline-minor-mode)
+;; #+end_example
 
 ;; If you want a different prefix key for outline-minor-mode, insert first:
 
-;; # #+begin_src emacs-lisp
-;; #  (defvar outline-minor-mode-prefix "\C-c")
-;; # #+end_src
+;; #+begin_example
+;;  (defvar outline-minor-mode-prefix "\C-c")
+;; #+end_example
 
 ;; or
 
-;; # #+begin_src emacs-lisp
-;; #  (defvar outline-minor-mode-prefix "\M-#")
-;; # #+end_src
+;; #+begin_example
+;;  (defvar outline-minor-mode-prefix "\M-#")
+;; #+end_example
 
-;; or whatever. The prefix can only be changed before outline (minor) mode is
-;; loaded.
+;; or whatever. The prefix can only be changed before outline
+;; (minor) mode is loaded.
 
 ;;;;; Emacs Version
 
-;; `outshine.el' works with [GNU Emacs 24.2.1 (x86_64-unknown-linux-gnu, GTK+
-;; Version 3.6.4) of 2013-01-20 on eric]. No attempts of testing with older
-;; versions or other types of Emacs have been made (yet).
+;; `outshine.el' works with [GNU Emacs 24.2.1
+;; (x86_64-unknown-linux-gnu, GTK+ Version 3.6.4) of 2013-01-20 on
+;; eric]. No attempts of testing with older versions or other types
+;; of Emacs have been made (yet).
 
 
 ;;;; ChangeLog
